56b16ee5db5a6665ebc731ec5a72080fde6fdec3
Jiang Xu <1067774901@qq.com>
2020-11-25 18:54:36 +0800

    学完数据类型和变量

--- C/src/minipro/main.c
+++ C/src/minipro/main.c
@@ -3,7 +3,10 @@
 
 #include "proj.h"
 
+int i = 10;
+
 int main(){
 	printf("[%s]:i = %d\n", __FUNCTION__, i);
+	call_func();
 	exit(0);
 }
---
+++ C/src/minipro/proj.h
@@ -0,0 +1,6 @@
+#ifndef PROJ_H__
+#define PROJ_H__
+
+static void func(void);
+
+#endif
